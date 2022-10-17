b41f31ea544ff00efb4a09f6f180b303e37a1ec4
João Alberto <joaoalbertosv15@gmail.com>
2022-10-17 07:28:11 -0300

    first commit

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,18 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      "modules-resolver",
+      {
+        root: ['./src'],
+        alias: {
+          "@assets": "./src/assets",
+          "@components": "./src/components",
+          "@screens": "./src/screens",
+          "@storage": "./src/storage",
+          "@utils": "./src/utils",
+        },
+      },
+    ],
   };
 };
