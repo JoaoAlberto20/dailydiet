79b3576b2c862c402c0f4a241365e45fb5e1315c
João Alberto <joaoalbertosv15@gmail.com>
2022-10-17 08:01:57 -0300

    :heavy_plus_sign: feat: Adicionando depedência de fonts

---
+++ src/themes/index.tsx
@@ -0,0 +1,37 @@
+export default {
+
+  COLORS: {
+    RED_700: '#BF3B44',
+    RED_300: '#F3BABD',
+    RED_100: '##F4E6E7',
+  
+    GREEN_700: '#639339',
+    GREEN_300: '#CBE4B4',
+    GREEN_100: '#E5F0DB',
+  
+    GRAY_700: '#1B1D1E',
+    GRAY_600: '#333638',
+    GRAY_500: '#5C6265',
+    GRAY_400: '#B9BBBC',
+    GRAY_300: '#DDDEDF',
+    GRAY_200: '#DDDEDF',
+    GRAY_100: '#FAFAFA',
+  
+    WHITE: '#FFFFFF',
+  },
+
+  FONT_SIZE: {
+    XS: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+    '2XL': 32,
+  },
+
+  FONT_FAMILY: {
+    REGULAR: 'NunitoSans_400Regular',
+    BOLD: 'NunitoSans_700Bold'
+  },
+
+} as const;
